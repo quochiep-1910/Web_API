d8545d0488995d27039c3e83e93c3bec275fdd12
quochiep-1910 <hiepgakute1019@gmail.com>
2020-12-24 09:36:43 +0700

    Add data to sql

---
+++ WebAPI.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using WebAPI.Data.Infrastructure;
+using WebAPI.Model.Models;
+
+namespace WebAPI.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        //tất cả phương thức update, delete, add đã tự động tạo bởi lớp kế thừa
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
