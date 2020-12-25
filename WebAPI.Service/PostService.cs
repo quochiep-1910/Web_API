e5675d7e4566d5a92b3e4154e5f5b8921db38631
quochiep-1910 <hiepgakute1019@gmail.com>
2020-12-25 07:33:27 +0700

    .

---
+++ WebAPI.Service/PostService.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using WebAPI.Data.Infrastructure;
+using WebAPI.Data.Repositories;
+using WebAPI.Model.Models;
+
+namespace WebAPI.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+
+        void Update(Post post);
+
+        void Delete(int id);
+
+        IEnumerable<Post> GetAll(); //IEnumerable giúp một đối tượng có thể thực hiện duyệt các phần tử bằng foreach.
+
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int TotalRow);
+
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow);
+
+        void SaveChanges();
+    }
+
+    public class PostService : IPostService
+    {
+        private IPostRepository _postRepository;
+
+        private IUnitOfWork _unitOfWork;
+
+        public PostService(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            this._postRepository = postRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(Post post)
+        {
+            _postRepository.Add(post);
+        }
+
+        public void Delete(int id)
+        {
+            _postRepository.Delete(id);
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            //lấy ra danh sách của Post tương ứng với PostCategory
+            return _postRepository.GetAll(new string[] { "PostCategory" });
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(string tag, int page, int pageSize, out int totalRow)
+        {
+            //TODO: Select all post by tag
+            return _postRepository.GetAllByTag(tag, page, pageSize, out totalRow);
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            return _postRepository.GetMultiPaging(x => x.Status, out totalRow, page, pageSize);
+        }
+
+        public Post GetById(int id)
+        {
+            return _postRepository.GetSingleById(id);
+        }
+
+        public void SaveChanges()
+        {
+            _unitOfWork.Commit();
+        }
+
+        public void Update(Post post)
+        {
+            _postRepository.Update(post);
+        }
+    }
+}
