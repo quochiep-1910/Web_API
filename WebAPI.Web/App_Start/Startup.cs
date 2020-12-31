4e3f123ee2f406088d558562ee43d81d60bf63ba
quochiep-1910 <hiepgakute1019@gmail.com>
2020-12-31 19:51:27 +0700

    Identity

---
+++ WebAPI.Web/App_Start/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+using WebAPI.Data;
+using WebAPI.Data.Infrastructure;
+using WebAPI.Data.Repositories;
+using WebAPI.Model.Models;
+using WebAPI.Service;
+
+[assembly: OwinStartup(typeof(WebAPI.Web.App_Start.Startup))]
+
+namespace WebAPI.Web.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+            ConfigureAuth(app);
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            // Register your Web API controllers.
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            //Register WebApi Controllers
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //Tự động khởi tạo Register(đăng kí)
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+
+            builder.RegisterType<GroceryDbContext>().AsSelf().InstancePerRequest();
+
+            //Asp.net Identity
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register(c => app.GetDataProtectionProvider()).InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            //Service(lấy ra tất cả các đối tượng có hậu tố với service)
+            builder.RegisterAssemblyTypes(typeof(PostCategoryService).Assembly)
+                .Where(t => t.Name.EndsWith("Service"))
+                 .AsImplementedInterfaces().InstancePerRequest(); //Lấy tất cả các Interface được khởi tạo. VD: IPostService và PostService
+
+            //gán tất cả register bên trên  vào container
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container)); // (phân giải)
+
+            //Set the WebApi DependencyResolver (phân giải)
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
+        }
+    }
+}
