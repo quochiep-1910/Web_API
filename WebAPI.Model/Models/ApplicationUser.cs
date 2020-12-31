a64cca6471f583aefa76db5278d75e938570bf07
quochiep-1910 <hiepgakute1019@gmail.com>
2020-12-31 19:51:53 +0700

    .

---
+++ WebAPI.Model/Models/ApplicationUser.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Model.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(256)]
+        public string FullName { set; get; }
+
+        [MaxLength(256)]
+        public string Address { set; get; }
+
+        public DateTime? BirthDay { set; get; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Lưu ý Loại xác thực phải khớp với Loại được xác định trong CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
