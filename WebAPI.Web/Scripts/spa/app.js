2fa469f8b2dc657c27815f878e3cf5f95dd8b714
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-01 20:17:39 +0700

    AngularJs

---
+++ WebAPI.Web/Scripts/spa/app.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../plugins/angular/angular.js" />
+var MyApp = angular.module('myModule', []);
+
+MyApp.controller("myController", myController);
+MyApp.controller("myController1", myController);
+
+//myController.$inject = ['$scope'];
+function myController($rootScope, $scope) {
+	$rootScope.message = "This is ";
+}
+function myController1($scope) {
+	$scope.message = "This is abc ";
+}
