83dfde4598e30c01462728c3c45144136ebecde1
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-03 16:51:12 +0700

    setting front end

---
+++ WebAPI.Web/app/app.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('grocery', ['grocery.products', 'grocery.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'] //tiêm thuộc tính có sẵn trong angular
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', { //khai báo cho đường dẫn admin và dùng controller
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin'); //otherwise là nếu ko thì trả về admin
+    }
+})();
---
+++ WebAPI.Web/app/components/products/products.module.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('grocery.products', ['grocery.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'] //tiêm thuộc tính có sẵn trong angular
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products', { //khai báo cho đường dẫn admin và dùng controller
+            url: "/products",
+            templateUrl: "/app/components/products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', { //khai báo cho đường dẫn admin và dùng controller
+            url: "/product_add",
+            templateUrl: "/app/components/products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
