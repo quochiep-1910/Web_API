d3a0efb78f21be922d3461a8dc8400e096339151
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-03 21:13:39 +0700

    add models product, tag, category

---
+++ WebAPI.Web/Models/ProductViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAPI.Model.Models;
+
+namespace WebAPI.Web.Models
+{
+    [Serializable]
+    public class ProductViewModel
+    {
+        public int ID { set; get; }
+
+        public string Name { set; get; }
+
+        public string Alias { set; get; }
+
+        public int CategoryID { set; get; }
+
+        public string Image { set; get; }
+
+        public string MoreImages { set; get; }
+
+        public decimal Price { set; get; }
+
+        public decimal? PromotionPrice { set; get; }
+
+        public int? Warranty { set; get; }
+
+        public string Description { set; get; }
+
+        public string Content { set; get; }
+
+        public bool? HomeFlag { set; get; }
+
+        public bool? HotFlag { set; get; }
+
+        public int? ViewCount { set; get; }
+
+        public string Tags { set; get; }
+
+        public int Quantity { set; get; }
+
+        public decimal OriginalPrice { set; get; }
+
+        public virtual ProductCategory ProductCategory { set; get; }
+
+        public virtual IEnumerable<ProductTag> ProductTags { set; get; }
+    }
+}
