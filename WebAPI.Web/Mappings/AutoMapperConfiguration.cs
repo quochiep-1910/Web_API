d0c8c900b9abeef8bbaac6c05315bea92ef5e505
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-03 21:12:48 +0700

    add mapping

--- WebAPI.Web/Mappings/AutoMapperConfiguration.cs
+++ WebAPI.Web/Mappings/AutoMapperConfiguration.cs
@@ -17,9 +17,9 @@ namespace WebAPI.Web.Mappings
                 cfg.CreateMap<Post, PostViewModel>();
                 cfg.CreateMap<PostCategory, PostCategoryViewModel>();
                 cfg.CreateMap<Tag, TagViewModel>();
-                //cfg.CreateMap<ProductCategory, ProductCategoryViewModel>();
-                //cfg.CreateMap<Product, ProductViewModel>();
-                //cfg.CreateMap<ProductTag, ProductTagViewModel>();
+                cfg.CreateMap<ProductCategory, ProductCategoryViewModel>();
+                cfg.CreateMap<Product, ProductViewModel>();
+                cfg.CreateMap<ProductTag, ProductTagViewModel>();
                 //cfg.CreateMap<Footer, FooterViewModel>();
                 //cfg.CreateMap<Slide, SlideViewModel>();
                 //cfg.CreateMap<Page, PageViewModel>();
