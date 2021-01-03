8c6a5dddf853ccf25c1e23ddc89b6824c6040852
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-03 21:13:52 +0700

    add api service

---
+++ WebAPI.Web/app/shared/services/apiService.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../../assets/admin/libs/angular/angular.js" />
+
+(function (app) {
+    app.factory('apiService', apiService);
+    apiService.$inject = ['$http']; //cần inject nếu ko thì khi chạy chữ $ đổi thành chữ e gây lỗi
+    function apiService($http) {
+        return {
+            get: get //gọi lại func get ở dưới
+        }
+        function get(url, params, success, failure) {
+            $http.get(url, params).then(function (result)//then là xủ lý xog sau khi gọi
+            {
+                success(result); //nhận thông tin trả
+            }, function (error) {
+                failure(error); //bắt lỗi
+            });
+        }
+    }
+})(angular.module('grocery.common'));
