55fdd955a486d14e26bf75bb38803f029267215f
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-04 20:59:32 +0700

    .

--- WebAPI.Web/app/components/product_categories/producteCategoryListController.js
+++ WebAPI.Web/app/components/product_categories/producteCategoryListController.js
@@ -1,6 +1,20 @@
 ﻿(function (app) {
     app.controller('producteCategoryListController', producteCategoryListController);
 
-    function producteCategoryListController() {
+    producteCategoryListController.$inject = ['$scope', 'apiService'];
+
+    function producteCategoryListController($scope, apiService) {
+        $scope.productCategories = [];
+
+        $scope.getProductCategories = getProductCategories;
+
+        function getProductCategories() {
+            apiService.get('/api/productcategory/getall', null, function (result) { //dùng apiService để gọi
+                $scope.productCategories = result.data; //nhận kết quả từ reponse
+            }, function () {
+                console.log('Load productcategory failed.'); //ghi log
+            })
+        }
+        $scope.getProductCategories(); //chạy khi controller khởi tạo
     }
 })(angular.module('grocery.product_categories'));
