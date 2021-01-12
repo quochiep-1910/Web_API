122d62200ba0919d2801e5d49c1f6cc0e3012aa3
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-12 21:24:10 +0700

    /

--- WebAPI.Web/Models/ProductCategoryViewModel.cs
+++ WebAPI.Web/Models/ProductCategoryViewModel.cs
@@ -10,10 +10,10 @@ namespace WebAPI.Web.Models
     {
         public int ID { set; get; }
 
-        [Required]
+        [Required(ErrorMessage = "Yêu cầu nhập tên danh mục")]
         public string Name { set; get; }
 
-        [Required]
+        [Required(ErrorMessage = "Yêu cầu nhập Alias")]
         public string Alias { set; get; }
 
         public string Description { set; get; }
@@ -40,7 +40,7 @@ namespace WebAPI.Web.Models
 
         public string MetaDescription { set; get; }
 
-        [Required]
+        [Required(ErrorMessage = "Yêu cầu nhập trạng thái")]
         public bool Status { set; get; }
     }
 }
