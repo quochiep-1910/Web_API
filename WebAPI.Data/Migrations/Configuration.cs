a86eccf719919ac8534f32ef49fa26e18f7e6a90
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-22 07:42:43 +0700

    add seed slide

--- WebAPI.Data/Migrations/Configuration.cs
+++ WebAPI.Data/Migrations/Configuration.cs
@@ -6,6 +6,7 @@
     using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebAPI.Common;
     using WebAPI.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebAPI.Data.GroceryDbContext>
@@ -23,33 +24,37 @@
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
             CreateProductCategorySample(context);
+            CreateSlide(context);
+        }
 
-            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new GroceryDbContext()));
-            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new GroceryDbContext()));
+        private void CreateUser(GroceryDbContext context)
+        {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new GroceryDbContext()));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new GroceryDbContext()));
 
-            //var user = new ApplicationUser()
-            //{
-            //    UserName = "Grocery",
-            //    Email = "hiepgakute1111@gmail.com",
-            //    EmailConfirmed = true,
-            //    BirthDay = DateTime.Now,
-            //    FullName = "Nguyen Quoc Hiep"
-            //};
-            ////password User
-            //manager.Create(user, "quochieps@2");
+            var user = new ApplicationUser()
+            {
+                UserName = "Grocery",
+                Email = "hiepgakute1111@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Nguyen Quoc Hiep"
+            };
+            //password User
+            manager.Create(user, "quochieps@2");
 
-            ////nếu role chưa tồn tại thì tạo 2 role Admin và user
-            //if (!roleManager.Roles.Any())
-            //{
-            //    roleManager.Create(new IdentityRole { Name = "Admin" });
-            //    roleManager.Create(new IdentityRole { Name = "User" });
-            //}
+            //nếu role chưa tồn tại thì tạo 2 role Admin và user
+            if (!roleManager.Roles.Any())
+            {
+                roleManager.Create(new IdentityRole { Name = "Admin" });
+                roleManager.Create(new IdentityRole { Name = "User" });
+            }
 
-            ////tìm user admin thông qua Email
-            //var adminUser = manager.FindByEmail("hiepgakute1111@gmail.com");
+            //tìm user admin thông qua Email
+            var adminUser = manager.FindByEmail("hiepgakute1111@gmail.com");
 
-            ////nếu thành công thì có giá trị và add admin và user
-            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            //nếu thành công thì có giá trị và add admin và user
+            manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
         }
 
         private void CreateProductCategorySample(WebAPI.Data.GroceryDbContext context)
@@ -65,6 +70,29 @@
                     new ProductCategory() { Name = "Mỹ Phẩm", Alias = "my-pham", Status = false }
                 };
                 context.ProductCategories.AddRange(listProductCategory);
+                context.SaveChanges();
+            }
+        }
+
+        private void CreateFooter(GroceryDbContext context)
+        {
+            if (context.Footers.Count(x => x.ID == CommonConstants.DefaultFooterId) == 0)
+            {
+                string content = "";
+            }
+        }
+
+        private void CreateSlide(GroceryDbContext context)
+        {
+            if (context.Slides.Count() == 0)
+            {
+                List<Slide> listSlide = new List<Slide>()
+                {
+                    new Slide(){Name="Slide 1", DisplayOrder=1,Status=true,Url="#",Image="/Assets/client/img/slider/slide_1.png",Content=@"<h1>Women's Fashion</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><a href=""#"">shop now</a>"},
+                    new Slide(){Name="Slide 2", DisplayOrder=2,Status=true,Url="#",Image="/Assets/client/img/slider/slide_2.png",Content=@"<h1>New Collection</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><a href=""#"">shop now</a>"},
+                    new Slide(){Name="Slide 2", DisplayOrder=3,Status=true,Url="#",Image="/Assets/client/img/slider/slide_3.png",Content=@"<h1>Best Collection</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p><a href=""#"">shop now</a>"}
+                };
+                context.Slides.AddRange(listSlide);
                 context.SaveChanges();
             }
         }
