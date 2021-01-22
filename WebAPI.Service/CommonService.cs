0bb8ebc3fc5742a615755fad8f67377b6b6f1af8
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-22 07:42:17 +0700

    add commonService(footerService)

---
+++ WebAPI.Service/CommonService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Common;
+using WebAPI.Data.Infrastructure;
+using WebAPI.Data.Repositories;
+using WebAPI.Model.Models;
+
+namespace WebAPI.Service
+{
+    public interface ICommonService
+    {
+        Footer GetFooter();
+
+        IEnumerable<Slide> GetSlides();
+    }
+
+    public class CommonService : ICommonService
+    {
+        private IFooterRepository _footerRepository;
+
+        private IUnitOfWork _unitOfWork;
+
+        private ISlideRepository _slideRepository;
+
+        public CommonService(IFooterRepository footerRepository, IUnitOfWork unitOfWork, ISlideRepository slideRepository)
+        {
+            this._footerRepository = footerRepository;
+            this._unitOfWork = unitOfWork;
+            this._slideRepository = slideRepository;
+        }
+
+        public Footer GetFooter()
+        {
+            return _footerRepository.GetSingleByCondition(x => x.ID == CommonConstants.DefaultFooterId);
+        }
+
+        public IEnumerable<Slide> GetSlides()
+        {
+            return _slideRepository.GetMulti(x => x.Status);
+        }
+    }
+}
