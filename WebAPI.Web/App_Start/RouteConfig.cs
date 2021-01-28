55d08e9a941e5d5a6082e6af95ee5b7eeb88d8ee
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-28 10:14:29 +0700

    Routing url thân thiện

--- WebAPI.Web/App_Start/RouteConfig.cs
+++ WebAPI.Web/App_Start/RouteConfig.cs
@@ -10,6 +10,26 @@ namespace WebAPI.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+          name: "About",
+          url: "gioi-thieu.html",
+          defaults: new { controller = "About", action = "Index", id = UrlParameter.Optional }
+      );
+            routes.MapRoute(
+            name: "Login",
+            url: "dang-nhap.html",
+            defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+      );
+            routes.MapRoute(
+              name: "Product Category",
+              url: "{alias}.pc-{id}.html",
+              defaults: new { controller = "Product", action = "Category", id = UrlParameter.Optional }
+          );
+            routes.MapRoute(
+            name: "Product",
+            url: "{alias}-{id}-p.html",
+            defaults: new { controller = "Product", action = "Detail", id = UrlParameter.Optional }
+        );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
