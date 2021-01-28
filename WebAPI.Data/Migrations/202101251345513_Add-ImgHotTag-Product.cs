cf5e427cd846834cdaf8fee57a08b133c4bc1664
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-28 10:17:56 +0700

    add image HOt tag product

---
+++ WebAPI.Data/Migrations/202101251345513_Add-ImgHotTag-Product.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddImgHotTagProduct : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "ImageHotTag", c => c.String(maxLength: 256));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "ImageHotTag");
+        }
+    }
+}
