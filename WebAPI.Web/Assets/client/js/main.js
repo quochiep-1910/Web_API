3060ddac3d09c0db36590c410086a9f1a7cb4b08
quochiep-1910 <hiepgakute1019@gmail.com>
2021-01-28 10:15:39 +0700

    modify assets

--- WebAPI.Web/Assets/client/js/main.js
+++ WebAPI.Web/Assets/client/js/main.js
@@ -173,26 +173,26 @@
     /*------------------------------
      Category menu Activation
    ------------------------------*/
-    //$('.sidebar_widget.catrgorie > ul > li > a').on('click', function(){
-    //   $(this).removeAttr('href');
-    //   var element = $(this).parent('li');
-    //    if (element.hasClass('open')) {
-    //       element.removeClass('open');
+    $('.sidebar_widget.catrgorie > ul > li > a').on('click', function () {
+        $(this).removeAttr('href');
+        var element = $(this).parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
 
-    //       element.find('li').removeClass('open');
+            element.find('li').removeClass('open');
 
-    //       element.find('ul').slideUp();
-    //   }
-    //      else {
-    //        element.addClass('open');
-    //        element.children('ul').slideDown();
-    //        element.siblings('li').children('ul').slideUp();
-    //        element.siblings('li').removeClass('open');
-    //        element.siblings('li').find('li').removeClass('open');
-    //        element.siblings('li').find('ul').slideUp();
-    //      }
-    //     });
-    //     $('.has-sub > a').append('<span class="holder"></span>');
+            element.find('ul').slideUp();
+        }
+        else {
+            element.addClass('open');
+            element.children('ul').slideDown();
+            element.siblings('li').children('ul').slideUp();
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp();
+        }
+    });
+    $('.has-sub > a').append('<span class="holder"></span>');
 
     /*----------------------------
    slider-range here
